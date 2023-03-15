53cb6e8d705970718a63cd9e1e1dfea7a4ae7014
NoamBasha <72918745+NoamBasha@users.noreply.github.com>
2023-03-15 14:02:55 +0200

    Added DownloadFile to download the example file

--- client/src/components/DownloadFile.js
+++ client/src/components/DownloadFile.js
@@ -1,10 +1,12 @@
+import FileDownload from "js-file-download";
+
 export const DownloadFile = () => {
 	const handleClick = async (e) => {
-		//TODO: e.preventDefault();
+		e.preventDefault();
 		try {
-			let data = await fetch("http://localhost:1337/user_input_example");
-			let json_data = await data.json();
-			console.log(json_data);
+			const res = await fetch("http://localhost:1337/user_input_example");
+			const res_blob = await res.blob();
+			FileDownload(res_blob, "user_input_example_from_server.csv");
 		} catch (err) {
 			alert("Could not download file, server disconnected");
 		}
