16be872a1e5bba8a984b45e66e79f0b9d947f131
יונתן פנחס <yonatan.pinchas@live.biu.ac.il>
2023-04-16 15:17:39 +0300

    deploying app on heroku

--- server/index.js
+++ server/index.js
@@ -6,12 +6,13 @@ let mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const path = require("path");
 const crypto = require("crypto");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json({ limit: "50mb" }));
+app.use(express.static(path.join(__dirname + "/public")));
 
 const CONNECTION_URL =
 	"mongodb+srv://yonipini:cargoexpert@cluster0.jq9nixx.mongodb.net/?retryWrites=true&w=majority";
