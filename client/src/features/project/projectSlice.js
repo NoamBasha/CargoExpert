bcef59c3cc4d4e72ed6759dcb4167b8502c1f33c
NoamBasha <noamba10@gmail.com>
2023-11-09 01:30:45 +0200

    Used createSelector to memoize selectProjectContainer

--- client/src/features/project/projectSlice.js
+++ client/src/features/project/projectSlice.js
@@ -1,55 +1,56 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { logout } from "../auth/authSlice.js";
 import {
-	updateSolution,
-	deleteSolution,
-	improveSolution,
-	duplicateSolution
+    updateSolution,
+    deleteSolution,
+    improveSolution,
+    duplicateSolution,
 } from "../solution/solutionSlice.js";
+import { createSelector } from "@reduxjs/toolkit";
 
 const initialState = {
-	projectId: null,
-	name: "",
-	container: null,
-	boxes: [],
-	solutions: [],
+    projectId: null,
+    name: "",
+    container: null,
+    boxes: [],
+    solutions: [],
 };
 
 export const projectSlice = createSlice({
-	name: "project",
-	initialState,
-	reducers: {
-		setProjectById: (state, action) => {
-			const { projects, projectId } = action.payload;
-			const project = projects.find(
-				(project) => project._id === projectId
-			);
-			state.projectId = projectId;
-			state.name = project.name;
-			state.container = project.container;
-			state.boxes = project.boxes;
-			state.solutions = project.solutions;
-		},
-		reset: (state) => initialState,
-	},
-	extraReducers: (builder) => {
-		builder
-			.addCase(logout, () => {
-				return initialState;
-			})
-			.addCase(duplicateSolution.fulfilled, (state, action) => {
-				state.solutions = action.payload.solutions;
-			})
-			.addCase(updateSolution.fulfilled, (state, action) => {
-				state.solutions = action.payload.solutions;
-			})
-			.addCase(deleteSolution.fulfilled, (state, action) => {
-				state.solutions = action.payload.solutions;
-			})
-			.addCase(improveSolution.fulfilled, (state, action) => {
-				state.solutions = action.payload.project.solutions;
-			});
-	},
+    name: "project",
+    initialState,
+    reducers: {
+        setProjectById: (state, action) => {
+            const { projects, projectId } = action.payload;
+            const project = projects.find(
+                (project) => project._id === projectId
+            );
+            state.projectId = projectId;
+            state.name = project.name;
+            state.container = project.container;
+            state.boxes = project.boxes;
+            state.solutions = project.solutions;
+        },
+        reset: (state) => initialState,
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(logout, () => {
+                return initialState;
+            })
+            .addCase(duplicateSolution.fulfilled, (state, action) => {
+                state.solutions = action.payload.solutions;
+            })
+            .addCase(updateSolution.fulfilled, (state, action) => {
+                state.solutions = action.payload.solutions;
+            })
+            .addCase(deleteSolution.fulfilled, (state, action) => {
+                state.solutions = action.payload.solutions;
+            })
+            .addCase(improveSolution.fulfilled, (state, action) => {
+                state.solutions = action.payload.project.solutions;
+            });
+    },
 });
 
 export const { setProjectById, reset } = projectSlice.actions;
@@ -58,13 +59,14 @@ export default projectSlice.reducer;
 
 export const selectProjectId = (state) => state.project.projectId;
 export const selectProjectName = (state) => state.project.name;
-export const selectProjectContainer = (state) => {
-	const container = state.project.container
-	return {
-	width: container.width,
-	height: container.height,
-	length: container.length
-}
-};
+const selectContainerFromState = (state) => state.project.container;
+export const selectProjectContainer = createSelector(
+    [selectContainerFromState],
+    (container) => ({
+        width: container.width,
+        height: container.height,
+        length: container.length,
+    })
+);
 export const selectProjectBoxes = (state) => state.project.boxes;
 export const selectProjectSolutions = (state) => state.project.solutions;
